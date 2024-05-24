703a92ac37af8f0ee5cf276746c57b0219b18b2c
efe corukoğlu <efecorukoglu@gmail.com>
2024-05-24 18:58:43 +0300

    added menu drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,153 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const appTitle = 'Notification App';
   static const primaryColor = Color(0xFF424549);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notification App',
-      theme: ThemeData(
-        primaryColor: Color(0x424549),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Notification App'),
-          backgroundColor: primaryColor,
-          centerTitle: true,
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Option1',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Option2',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Option3',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Option4',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 5: Option5',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
         ),
-        body: Text('merhaba dünya'),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      drawer: Drawer(
+        //listview
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Option1'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Option2'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+      
+      /*
+theme: ThemeData(
+        primaryColor: Color(0x424549),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Notification App'),
+          backgroundColor: primaryColor,
+          centerTitle: true,
+        ),
+        drawer: Drawer(
+          child: Text('asd'), //todo,
+        ),
+        body: Text('merhaba dünya'),
+      ),
+      */
+      
